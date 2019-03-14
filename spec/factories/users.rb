79c65edb93b67c4d68e492bc265c69a772f093f5
沼幹太 <kanta.mischil@icloud.com>
2019-03-14 21:22:51 +0900

    fakerの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user do
-    email                 {"kkk@gmail.com"}
+    email                 {Faker::Internet.email}
     password              {"00000000"}
     password_confirmation {"00000000"}
   end
