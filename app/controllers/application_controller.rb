308a56293ca74f5e6a64c3cb2683d528aeed1e4c
沼幹太 <kanta.mischil@icloud.com>
2019-03-30 20:43:05 +0900

    ログイン認証　

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == 'admin' && password == '2222'
     end
   end
 end
-
